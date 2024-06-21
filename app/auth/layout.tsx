ff4d56ee258cadbc1c71ae611590524d2a0e14b7
mbaluev <mikhail.baluev@gmail.com>
2024-06-22 01:44:29 +0200

    login form styles

--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -1,16 +1,7 @@
 import { ReactNode } from 'react';
 
 const AuthLayout = ({ children }: { children: ReactNode }) => {
-  return (
-    <div
-      className="h-full flex items-center justify-center
-        {/*bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-gray-900 to-gray-950*/}
-        {/*bg-gray-200*/}
-        "
-    >
-      {children}
-    </div>
-  );
+  return <div className="min-h-full flex items-center justify-center p-4">{children}</div>;
 };
 
 export default AuthLayout;
