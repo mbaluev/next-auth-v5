11ef2617fd8d97a01e61c24e57901ecc1541d919
mbaluev <mikhail.baluev@gmail.com>
2024-09-10 15:19:32 +0200

    github actions

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
+  experimental: {
+    serverActions: {
+      allowedForwardedHosts: ['localhost'],
+      allowedOrigins: ['http://localhost']
+    },
+  }
 };
 
 export default nextConfig;
