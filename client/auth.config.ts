01b03deb7954eb587d083b161da6dc2b8ff48285
mbaluev <mikhail.baluev@gmail.com>
2024-09-11 16:04:57 +0200

    github actions

--- client/auth.config.ts
+++ client/auth.config.ts
@@ -10,12 +10,12 @@ export default {
   trustHost: true,
   providers: [
     Google({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      clientId: process.env.AUTH_GOOGLE_CLIENT_ID,
+      clientSecret: process.env.AUTH_GOOGLE_CLIENT_SECRET,
     }),
     GitHub({
-      clientId: process.env.GIT_CLIENT_ID,
-      clientSecret: process.env.GIT_CLIENT_SECRET,
+      clientId: process.env.AUTH_GITHUB_CLIENT_ID,
+      clientSecret: process.env.AUTH_GITHUB_CLIENT_SECRET,
     }),
     Credentials({
       async authorize(credentials) {
