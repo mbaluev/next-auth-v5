d3e16f9b82f0b86c340b1519446db2af2e6b2319
mbaluev <mikhail.baluev@gmail.com>
2024-09-23 00:36:47 +0200

    public folder

--- client/components/layout/header/settings-bar.tsx
+++ client/components/layout/header/settings-bar.tsx
@@ -1,12 +1,9 @@
 'use client';
 
-import { useCurrentUser } from '@/core/auth/hooks/use-current-user';
 import { SettingsThemeBtn } from '@/components/layout/header/settings-theme-btn';
 import { SettingsUserButton } from '@/components/layout/header/settings-user-button';
 
 export const SettingsBar = () => {
-  const user = useCurrentUser();
-  if (!user) return null;
   return (
     <nav className="flex-grow-0 flex gap-4">
       <SettingsThemeBtn />
