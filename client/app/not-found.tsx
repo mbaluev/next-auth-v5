83e2dd3a8deb6a5bbd95b9d6084c68df51962e2a
mbaluev <mikhail.baluev@gmail.com>
2024-09-22 14:10:13 +0200

    login dialog

--- client/app/not-found.tsx
+++ client/app/not-found.tsx
@@ -5,7 +5,7 @@ import { FormError } from '@/components/form-error';
 export default function NotFound() {
   return (
     <main className="flex h-full relative items-center justify-center">
-      <Image src="/not-found.jpg" alt="/not-found.jpg" objectFit="cover" fill className="z-[0]" />
+      <Image src="/static/not-found.jpg" alt="not-found" objectFit="cover" fill className="z-[0]" />
       <CardWrapper
         headerLabel="a simple authentication service"
         backButtonLabel="back home"
