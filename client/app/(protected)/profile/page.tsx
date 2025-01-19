9e38cc998833402e7fd746c9eb946f6f2703823d
mbaluev <mikhail.baluev@gmail.com>
2025-01-19 01:36:04 +0100

    refactor

--- client/app/(protected)/profile/page.tsx
+++ client/app/(protected)/profile/page.tsx
@@ -8,8 +8,8 @@ const ClientPage = () => {
   return (
     <div className="w-full grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
       <WidgetProfile />
-      <WidgetAdmin />
       <WidgetSettings />
+      <WidgetAdmin />
     </div>
   );
 };
