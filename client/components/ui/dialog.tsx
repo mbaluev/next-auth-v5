cccdfcaa7cecac802bfe55aa82e3e202006458a4
mbaluev <mikhail.baluev@gmail.com>
2025-01-19 01:08:02 +0100

    refactor dialog

--- client/components/ui/dialog.tsx
+++ client/components/ui/dialog.tsx
@@ -42,7 +42,14 @@ const DialogContent = React.forwardRef<
     <DialogPrimitive.Content
       ref={ref}
       className={cn(
-        'fixed left-[50%] translate-x-[-50%] top-24 z-50 grid w-full max-w-lg gap-6 bg-background p-6 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] rounded-lg',
+        'fixed top-24 z-50 left-[50%] translate-x-[-50%] duration-200',
+        'w-full max-w-lg grid gap-6 p-6',
+        'bg-background shadow-lg rounded-lg',
+        'data-[state=open]:animate-in data-[state=closed]:animate-out',
+        'data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0',
+        'data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95',
+        'data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%]',
+        'data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%]',
         className
       )}
       {...props}
