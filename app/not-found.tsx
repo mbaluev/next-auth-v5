4efa75b71404a2fc1aaa25caa9940f43d8f78014
mbaluev <mikhail.baluev@gmail.com>
2025-06-15 01:06:18 +0200

    not-found.tsx

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,17 +1,18 @@
-import { WidgetWrapper } from '@/components/auth/widget-wrapper';
-import { AlertError } from '@/components/ui/alert';
 import { MasterCenter } from '@/components/layout/master';
+import { Frown } from 'lucide-react';
+import { ButtonBack } from '@/components/auth/button-back';
 
 export default function NotFound() {
   return (
     <MasterCenter>
-      <WidgetWrapper
-        headerLabel="authentication service"
-        backButtonLabel="back home"
-        backButtonHref="/"
-      >
-        <AlertError message="page not found" />
-      </WidgetWrapper>
+      <div className="flex flex-col gap-10 items-center w-full py-10">
+        <Frown className="text-9xl text-muted-foreground" />
+        <div className="flex flex-col gap-3 items-center">
+          <p className="text-6xl font-medium">404</p>
+          <p>page not found</p>
+        </div>
+        <ButtonBack href="/" label="back home" />
+      </div>
     </MasterCenter>
   );
 }
